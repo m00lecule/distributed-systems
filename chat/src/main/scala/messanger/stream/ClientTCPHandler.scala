ad1800bd721ae4504cd773df18ca799bc1ed3079
mdygas <mdygas@saba.com>
2020-03-10 00:56:33 +0100

    TCP server reset connection fix

--- chat/src/main/scala/messanger/stream/ClientTCPHandler.scala
+++ chat/src/main/scala/messanger/stream/ClientTCPHandler.scala
@@ -7,12 +7,40 @@ import messanger.{Processable, Server}
 
 class ClientTCPHandler(override val socket: Socket, val server: Server) extends StreamObjectOperations with Processable[Socket] {
 
-  override def processMessage(message: MessageRef, sender: Socket): Unit = server.clientsTCP.stream().filter(!_.equals(this)).forEach(_.writeObject(message))
+  override def processMessage(message: MessageRef, sender: Socket): Unit = {
+    server.synchronized {
+      val it = server.clientsTCP.iterator
+      while(it.hasNext){
+        val client = it.next
+        if(client != this){
+          try{
+            client.writeObject(message)
+          }catch {
+            case _: Throwable =>
+              client.isRunning = false
+              server.clientsTCP.remove(client)
+              println(server.clientsTCP)
+          }
+        }
+      }
+    }
+  }
 
   override def processLogout(sender: Socket) = {
     isRunning = false
     server.clientsTCP.remove(this)
     println(server.clientsTCP)
+  }
+
+  override def processMessages: Unit = {
+    try{
+      super.processMessages
+    }catch {
+      case _: Throwable =>
+        isRunning = false
+        server.clientsTCP.remove(this)
+        println(server.clientsTCP)
+    }
   }
 }
 
